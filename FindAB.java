f45581e27126cd85f59ab9f725de179fb178a355
HeQYT <34596335+HeQYT@users.noreply.github.com>
2019-01-03 19:36:30 +0800

    Update FindAB.java

--- FindAB.java
+++ FindAB.java
@@ -1,3 +1,5 @@
+#20190102 无法求解，无理数
+
 public class FindAB{
  public static void main(String[] args){
   double i=0.62;
@@ -6,7 +8,7 @@ public class FindAB{
     while(j<1.8){
         if(2*i + j == 3 && i*j == j-i)
             System.out.println("i and j is:"+i+j);  //("i and j",i,j)
-        i = i+0.0000000000001;
+   #     i = i+0.0000000000001;
         j = j+0.0000000000001;
 	}
   }
