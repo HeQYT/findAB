ab7201dc871f234bae0c9db4293d5ec5ac95abf5
HeQYT <34596335+HeQYT@users.noreply.github.com>
2019-01-03 19:33:08 +0800

    Add files via upload

---
+++ FindAB.java
@@ -0,0 +1,15 @@
+public class FindAB{
+ public static void main(String[] args){
+  double i=0.62;
+  double j=1.0;
+  while(i<0.65){
+    while(j<1.8){
+        if(2*i + j == 3 && i*j == j-i)
+            System.out.println("i and j is:"+i+j);  //("i and j",i,j)
+        i = i+0.0000000000001;
+        j = j+0.0000000000001;
+	}
+  }
+  System.out.println("\nHava no answer!\n");
+ }
+}
